be2968a13a6a3a4d7b484f2a6a4a58b6095e5755
Tim May <11429163+ShoelessTim@users.noreply.github.com>
2026-07-29 07:12:37 -0400

    razerstream 1.7.2

--- Casks/razerstream.rb
+++ Casks/razerstream.rb
@@ -1,6 +1,6 @@
 cask "razerstream" do
-  version "1.7.0"
-  sha256 "5cc995cff79ee89bdd5c659d80e5b810b1ef5dc6e8d2a0b95b3e79ff54ee10a5"
+  version "1.7.2"
+  sha256 "42762aa560a006b123c410717d05077060e6b4e850362e53009802869c3686c3"
 
   url "https://github.com/ShoelessTim/RazerStream/releases/download/v#{version}/RazerStream-v#{version}.zip"
   name "RazerStream"
